35590a1b30b22ac102fa0ecf4461d848ce73a3f2
Sparkeyy <lukasnagel99@gmail.com>
2016-11-19 02:40:17 +0100

    Added Code for Group class

--- group/redirect.php
+++ group/redirect.php
@@ -4,33 +4,28 @@
 		http_response_code( 302 );
 		header( "Location: /Signin/");
 	} else {
-		$user = $user["user_id"];
 		if( substr(explode( "?",$_SERVER['REQUEST_URI'] )[0], -1) != "/"  ) {
 			http_response_code( 302 );
 			header( "Location: ".$_SERVER['REQUEST_URI']."/");
 		}
 		else {
-			$usr = explode( "/", explode( "?",$_SERVER['REQUEST_URI'] )[0] );
-			if( $usr[3] == "edit" ) {
-				if( $usr[2] != Login::checkUser()["user_id"] ) {
+			$user = $user["user_id"];
+			$url = explode( "/", explode( "?",$_SERVER['REQUEST_URI'] )[0] );
+			if( $url[3] == "edit" ) {
+				if( !Group::isMod( $user ) ) {
 					http_response_code( 302 );
 					header( "Location: /profile/".$usr[2] );
 					return;
 				}
-				   define( "PROFILEEDIT", true );
-			} else define( "PROFILEEDIT", false );
+				   define( "GROUPEDIT", true );
+			} else define( "GROUPEDIT", false );
 			
-			$usr = $usr[2];
-			if( $usr == "me" ) {
-				http_response_code( 302 );
-				header( "Location: /profile/$user/");
-			} else {
 		
 				http_response_code( 200 );
 				define( "CMSLOADSUBTPL", "profile.tpl" );
-				define( "PROFILEUSR", $usr );
+				define( "GROUPUSR", $url[2] );
 				require_once $_SERVER["DOCUMENT_ROOT"]."/Composer/composer.tpl";
-			}
+			
 		}
 		
 	}
