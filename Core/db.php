3fa041bd12c8f60febef91e6fd3c6ef85dfaee9e
Sparkeyy <lukasnagel99@gmail.com>
2016-12-01 15:33:20 +0100

    Fixed Login Error for double named persons

--- Core/db.php
+++ Core/db.php
@@ -1,10 +1,10 @@
 <?php
-	define('MYSQL_BOTH',MYSQLI_BOTH);
-	define('MYSQL_NUM',MYSQLI_NUM);
-	define('MYSQL_ASSOC',MYSQLI_ASSOC);
+	if( !defined(MYSQL_BOTH ) ) define('MYSQL_BOTH', MYSQLI_BOTH );
+	if( !defined(MYSQL_NUM  ) ) define('MYSQL_NUM',  MYSQLI_NUM  );
+	if( !defined(MYSQL_ASSOC) ) define('MYSQL_ASSOC',MYSQLI_ASSOC);
 	
 	function connectDB($tb = 'yearbook') {
-		$db = new mysqli('localhost', 'root', 'Berlingo1998', $tb);
+		$db = new mysqli('localhost', 'root', 'Sparky2000', $tb);
 		if( $db->connect_errno ) {
 			error_log( $db->connect_error );
 			return False;
