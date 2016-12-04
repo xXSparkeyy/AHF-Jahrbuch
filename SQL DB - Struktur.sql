3c9c4b1dd0175922f25dc944cecb9629b90aaa8e
Sparkeyy <lukasnagel99@gmail.com>
2016-12-04 19:12:56 +0100

    Updated SQL dump

--- SQL DB - Struktur.sql
+++ SQL DB - Struktur.sql
@@ -1,45 +1,68 @@
--- phpMyAdmin SQL Dump
--- version 4.0.10deb1
--- http://www.phpmyadmin.net
---
--- Host: localhost
--- Erstellungszeit: 09. Nov 2016 um 21:43
--- Server Version: 5.5.53-0ubuntu0.14.04.1
--- PHP-Version: 5.5.9-1ubuntu4.20
-
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8 */;
 
---
--- Datenbank: `yearbook`
---
 CREATE DATABASE IF NOT EXISTS `yearbook` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
 USE `yearbook`;
 
--- --------------------------------------------------------
+DROP TABLE IF EXISTS `group_meta`;
+CREATE TABLE IF NOT EXISTS `group_meta` (
+  `group_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` tinytext NOT NULL,
+  `description` text NOT NULL,
+  PRIMARY KEY (`group_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COMMENT='latin1_swedish_ci' AUTO_INCREMENT=3 ;
 
---
--- Tabellenstruktur für Tabelle `login_admin`
---
+DROP TABLE IF EXISTS `group_participants`;
+CREATE TABLE IF NOT EXISTS `group_participants` (
+  `group_participants_id` int(11) NOT NULL AUTO_INCREMENT,
+  `group_id` int(11) NOT NULL,
+  `user_id` text NOT NULL,
+  `mod` tinyint(1) NOT NULL,
+  PRIMARY KEY (`group_participants_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COMMENT='latin1_swedish_ci' AUTO_INCREMENT=10 ;
+
+DROP TABLE IF EXISTS `image_galleries`;
+CREATE TABLE IF NOT EXISTS `image_galleries` (
+  `gallery_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  PRIMARY KEY (`gallery_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='latin1_swedish_ci' AUTO_INCREMENT=1 ;
+
+DROP TABLE IF EXISTS `images_linking`;
+CREATE TABLE IF NOT EXISTS `images_linking` (
+  `images_linking_id` int(11) NOT NULL AUTO_INCREMENT,
+  `image_id` int(11) NOT NULL,
+  `gallery_id` int(11) NOT NULL,
+  PRIMARY KEY (`images_linking_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='latin1_swedish_ci' AUTO_INCREMENT=1 ;
+
+DROP TABLE IF EXISTS `images_path`;
+CREATE TABLE IF NOT EXISTS `images_path` (
+  `image_id` int(11) NOT NULL AUTO_INCREMENT,
+  `path` text NOT NULL,
+  PRIMARY KEY (`image_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='latin1_swedish_ci' AUTO_INCREMENT=1 ;
+
+DROP TABLE IF EXISTS `log`;
+CREATE TABLE IF NOT EXISTS `log` (
+  `log_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `content` text NOT NULL,
+  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`log_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COMMENT='latin1_swedish_ci' AUTO_INCREMENT=60 ;
 
 DROP TABLE IF EXISTS `login_admin`;
 CREATE TABLE IF NOT EXISTS `login_admin` (
   `uselesscol` int(11) NOT NULL AUTO_INCREMENT,
   `login_admin_id` text NOT NULL,
   PRIMARY KEY (`uselesscol`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COMMENT='latin1_swedish_ci' AUTO_INCREMENT=3 ;
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `login_info`
---
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COMMENT='latin1_swedish_ci' AUTO_INCREMENT=7 ;
 
 DROP TABLE IF EXISTS `login_info`;
 CREATE TABLE IF NOT EXISTS `login_info` (
@@ -47,37 +70,12 @@ CREATE TABLE IF NOT EXISTS `login_info` (
   `password` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='latin1_swedish_ci';
 
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `login_tokens`
---
-
 DROP TABLE IF EXISTS `login_tokens`;
 CREATE TABLE IF NOT EXISTS `login_tokens` (
   `user_id` text NOT NULL,
   `token` text NOT NULL,
   `expires` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='latin1_swedish_ci';
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `profiles`
---
-
-DROP TABLE IF EXISTS `profiles`;
-CREATE TABLE IF NOT EXISTS `profiles` (
-  `user_id` text NOT NULL,
-  `FName` text NOT NULL,
-  `LName` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='latin1_swedish_ci';
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `profile_meta_fields`
---
 
 DROP TABLE IF EXISTS `profile_meta_fields`;
 CREATE TABLE IF NOT EXISTS `profile_meta_fields` (
@@ -87,13 +85,7 @@ CREATE TABLE IF NOT EXISTS `profile_meta_fields` (
   `field_opt` text,
   `field_order` int(11) DEFAULT NULL,
   PRIMARY KEY (`field_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COMMENT='latin1_swedish_ci' AUTO_INCREMENT=5 ;
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `profile_user_fields`
---
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COMMENT='latin1_swedish_ci' AUTO_INCREMENT=12 ;
 
 DROP TABLE IF EXISTS `profile_user_fields`;
 CREATE TABLE IF NOT EXISTS `profile_user_fields` (
@@ -102,13 +94,15 @@ CREATE TABLE IF NOT EXISTS `profile_user_fields` (
   `user_id` text,
   `value` text,
   PRIMARY KEY (`uf_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COMMENT='latin1_swedish_ci' AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COMMENT='latin1_swedish_ci' AUTO_INCREMENT=23 ;
 
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `survey_meta`
---
+DROP TABLE IF EXISTS `profiles`;
+CREATE TABLE IF NOT EXISTS `profiles` (
+  `user_id` text NOT NULL,
+  `FName` text NOT NULL,
+  `LName` text NOT NULL,
+  `avatar` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='latin1_swedish_ci';
 
 DROP TABLE IF EXISTS `survey_meta`;
 CREATE TABLE IF NOT EXISTS `survey_meta` (
@@ -119,25 +113,13 @@ CREATE TABLE IF NOT EXISTS `survey_meta` (
   PRIMARY KEY (`survey_meta_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COMMENT='latin1_swedish_ci' AUTO_INCREMENT=6 ;
 
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `survey_questions`
---
-
 DROP TABLE IF EXISTS `survey_questions`;
 CREATE TABLE IF NOT EXISTS `survey_questions` (
   `survey_id` int(11) NOT NULL,
   `question_id` int(11) NOT NULL AUTO_INCREMENT,
   `question_title` text NOT NULL,
   PRIMARY KEY (`question_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COMMENT='latin1_swedish_ci' AUTO_INCREMENT=26 ;
-
--- --------------------------------------------------------
-
---
--- Tabellenstruktur für Tabelle `survey_votes`
---
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COMMENT='latin1_swedish_ci' AUTO_INCREMENT=11 ;
 
 DROP TABLE IF EXISTS `survey_votes`;
 CREATE TABLE IF NOT EXISTS `survey_votes` (
@@ -145,8 +127,9 @@ CREATE TABLE IF NOT EXISTS `survey_votes` (
   `vote_value` int(11) NOT NULL,
   `vote_user` text NOT NULL,
   `vote_question` int(11) NOT NULL,
+  `survey__id` int(11) NOT NULL,
   PRIMARY KEY (`vote_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COMMENT='latin1_swedish_ci' AUTO_INCREMENT=34 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 COMMENT='latin1_swedish_ci' AUTO_INCREMENT=5 ;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
